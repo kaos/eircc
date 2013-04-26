cdb3dc05db6093a99932c01669c7dd6b67871915
Andreas Stenius <git@astekk.se>
2013-04-26 14:49:49 +0200

    rename irc_bot -> irc_client. Start clients from eircc_sup.

--- src/eircc_sup.erl
+++ src/eircc_sup.erl
@@ -4,13 +4,13 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_client/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -19,10 +19,24 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_client(Options) ->
+    supervisor:start_child(
+      ?MODULE, 
+      [proplists:get_value(Key, Options, Def) 
+       || {Key, Def} <-
+              [{host, "localhost"},
+               {port, 7000},
+               {nick, "eircc"},
+               {pass, undefined},
+               {name, "Erlang IRC Client"}
+              ]
+      ]).
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, { {simple_one_for_one, 0, 1}, 
+           [?CHILD(irc_client, worker)]} }.
 
